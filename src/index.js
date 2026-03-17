354f238eb1a6e5e2f921e3ff7e3f18314abe7d4e
Leticia Leal <leticia.leal@growdev.academy>
2026-03-16 21:54:27 -0300

    feat: add configurações de cors para deploy da api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // termo para importar recursos que estão fora deste arquivo
 // const express = require('express') //CommonJS
 import express from 'express'; // ESModule
+import cors from 'cors';
 import leadsRoutes from './routes/leads.routes.js';
 
 
@@ -8,6 +9,8 @@ import leadsRoutes from './routes/leads.routes.js';
 const app = express()
 const port = 3000
 app.use(express.json())
+app.use(cors()); // API pública - Aceita requisições de qualquer Origem
+// Access-Control-Allow-Origin: *
 
 
 app.get("/", (req, res) => {
